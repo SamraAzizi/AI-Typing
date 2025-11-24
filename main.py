c9a386d958e7102e57709685186240ce70caab1d
SamraAzizi <samarraazizi@gmail.com>
2025-11-24 11:08:50 +0500

    ai typing corrector

---
+++ main.py
@@ -0,0 +1,17 @@
+import time
+from string import Template
+
+import httpx
+from pynput import keyboard
+from pynput.keyboard import Key, Controller
+import pyperclip
+
+
+controller = Controller()
+
+OLLAMA_ENDPOINT = "http://localhost:11434/api/generate"
+OLLAMA_CONFIG = {
+    "model": "mistral:7b-instruct-v0.2-q4_K_S",
+    "keep_alive": "5m",
+    "stream": False,
+}
